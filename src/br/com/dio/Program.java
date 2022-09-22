df2ebb2615a55d9864ed393b1c4a03945cd47d33
Teigo1383 <teiweb58@gmail.com>
2022-09-21 21:49:49 -0300

    Criacao de calculadora

---
+++ src/br/com/dio/Program.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(" Rodolfo", " Branco e Preto", 11);
+		
+		System.out.println(gato);
+
+	}
+
+}
